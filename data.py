ae49936eeca06cf0ce30bdf43f9912b21af6ab08
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-10-01 23:48:02 -0400

    Wrangling dates. Adds time structure and some additional fields.

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import json
+import time
 
 from bs4 import BeautifulSoup
+from dateutil.parser import *
+from pytz import timezone
 import requests
 
 def download_schedule_html():
@@ -13,42 +16,84 @@ def parse_schedule_html():
     with open('data/schedule.html', 'rb') as readfile:
         soup = BeautifulSoup(readfile.read())
 
+    eastern = timezone('US/Eastern')
+    DATES = ['Thursday, October 17', 'Friday, October 18', 'Saturday, October 19']
     output = []
 
     for klass in ['make', 'listen', 'solve']:
+
+        day_slice = 0
+        am = True
+
         for row in soup.select('tr.%s' % klass):
             session_dict = {}
+            session_dict['time'] = {}
 
             for index, cell in enumerate(row.select('td')):
                 if index == 0:
-                    session_dict['start_time'] = cell\
+                    session_dict['time']['start_time_string'] = cell\
                                                 .text\
                                                 .split('\n')[0]\
                                                 .replace(u'\u2013', '-')\
                                                 .split('-')[0]\
                                                 .strip()
                     try:
-                        session_dict['end_time'] = cell\
+                        session_dict['time']['end_time_string'] = cell\
                                                     .text\
                                                     .split('\n')[0]\
                                                     .replace(u'\u2013', '-')\
                                                     .split('-')[1]\
                                                     .strip()
                     except IndexError:
-                        session_dict['end_time'] = None
+                        session_dict['time']['end_time_string'] = None
 
                     session_dict['session_type'] = cell.text.split('\n')[1].strip()
                     session_dict['session_section'] = klass
 
-                    if session_dict['end_time']:
-                        if 'p.m.' in session_dict['end_time']:
-                            if 'a.m.' not in session_dict['start_time']:
-                                session_dict['start_time'] = '%s p.m.' % session_dict['start_time']
+                    if session_dict['time']['end_time_string']:
+                        if 'p.m.' in session_dict['time']['end_time_string']:
+                            if 'a.m.' not in session_dict['time']['start_time_string']:
+                                session_dict['time']['start_time_string'] = '%s p.m.' % session_dict['time']['start_time_string']
 
-                        if 'a.m.' in session_dict['end_time']:
-                        	session_dict['start_time'] = '%s a.m.' % session_dict['start_time']
+                        if 'a.m.' in session_dict['time']['end_time_string']:
+                            session_dict['time']['start_time_string'] = '%s a.m.' % session_dict['time']['start_time_string']                           
 
-                    session_dict['start_time'] = session_dict['start_time'].replace('p.m. p.m.', 'a.m.')
+                    session_dict['time']['start_time_string'] = session_dict['time']['start_time_string'].replace('p.m. p.m.', 'a.m.')
+
+                    if 'p.m.' in session_dict['time']['start_time_string']:
+                        if am is True:
+                            am = False
+
+                    if 'a.m.' in session_dict['time']['start_time_string']:
+                        if am is False:
+                            am = True
+                            day_slice += 1
+
+                    session_dict['time']['date_string'] = DATES[day_slice]
+
+                    session_dict['time']['start_timestamp'] = eastern.localize(
+                                                        parse("%s %s" % (
+                                                            session_dict['time']['date_string'],
+                                                            session_dict['time']['start_time_string']),
+                                                        ignoretz=True))
+                    session_dict['time']['start_timestamp'] = time.mktime(session_dict['time']['start_timestamp'].timetuple())
+
+                    if session_dict['time']['end_time_string']:
+
+                        if ':' not in session_dict['time']['end_time_string']:
+                            if 'p.m.' in session_dict['time']['end_time_string']:
+                                session_dict['time']['end_time_string'] = session_dict['time']['end_time_string'].split(' p.m.')[0] + ':00 p.m.'
+                            if 'a.m.' in session_dict['time']['end_time_string']:
+                                session_dict['time']['end_time_string'] = session_dict['time']['end_time_string'].split(' a.m.')[0] + ':00 a.m.'
+
+                        session_dict['time']['end_timestamp'] = eastern.localize(
+                                                            parse("%s %s" % (
+                                                                session_dict['time']['date_string'],
+                                                                session_dict['time']['end_time_string']),
+                                                            ignoretz=True))
+
+                        session_dict['time']['end_timestamp'] = time.mktime(session_dict['time']['end_timestamp'].timetuple())
+
 
                 if index == 1:
 
@@ -68,9 +113,9 @@ def parse_schedule_html():
                             session_dict['leaders'].append(leader_dict)
 
                     except IndexError:
-                    	pass
+                        pass
 
             output.append(session_dict)
 
     with open('data/schedule.json', 'wb') as writefile:
-    	writefile.write(json.dumps(output))
+        writefile.write(json.dumps(output))
