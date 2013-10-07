056626881cffad23b975aa344900dd5913bb8042
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-10-06 22:09:23 -0400

    Adds restaurants. Adds scraper/parser for restaurants.

--- data.py
+++ data.py
@@ -6,6 +6,101 @@ from dateutil.parser import *
 from pytz import timezone
 import requests
 
+def clean(text, **kwargs):
+    """
+    Clean is better than dirty.
+    Strips bad characters and
+    other marks (breaks, etc)
+    from text; returns clean.
+    """
+    BAD_CHARS = [
+        (u'\u201c', '"'),
+        (u'\u201d', '"'),
+        (u'\u2019', "'"),
+        (u'\u2014', '--'),
+        (u'\u2026', ''),
+    ]
+
+    if kwargs.get('breaks', None) != True:
+        BAD_CHARS.append((u'\n', ''))
+        BAD_CHARS.append((u'\r', ''))
+
+    for bad, good in BAD_CHARS:
+        text = text.replace(bad, good)
+
+    return text.strip()
+
+def clean_restaurant_descriptions():
+    cleaned = []
+    with open('data/restaurants.json', 'rb') as readfile:
+        restaurants = json.loads(readfile.read())
+
+    for restaurant in restaurants:
+        if restaurant['full_description']:
+            if 'Executive chef: ' in restaurant['full_description']:
+                restaurant['full_description'] = restaurant['full_description'].split('Executive chef: ')[0]
+        cleaned.append(restaurant)
+
+    with open('data/clean_restaurants.json', 'wb') as writefile:
+        writefile.write(json.dumps(cleaned))
+
+def parse_restaurant_html():
+    ROOT_URL = 'http://entertainment.accessatlanta.com'
+
+    restaurants = []
+
+    for star in [3,4,5]:
+        with open('data/%s-stars.html' % star, 'rb') as readfile:
+            soup = BeautifulSoup(readfile.read())
+
+        results = soup.select('td.search_result_content')
+        for result in results:
+            restaurant_dict = {}
+
+            restaurant_dict['stars'] = star
+            restaurant_dict['title'] = clean(result.select('td.title_content a')[0].text)
+            restaurant_dict['detail_url'] = ROOT_URL + result.select('td.title_content a')[0].attrs['href']
+            restaurant_dict['short_description'] = clean(result.select('td.description_content div')[0].text)
+            restaurant_dict['address'] = clean(result.select('td.meta_content')[0].text.split('\n\n')[2])
+            restaurant_dict['city'] = 'Atlanta'
+            restaurant_dict['state'] = 'GA'
+
+            restaurant_dict['phone'] = None
+            try:
+                if '(' in result.select('td.meta_content')[0].text.split('\n\n')[4]:
+                    restaurant_dict['phone'] = clean(result.select('td.meta_content')[0].text.split('\n\n')[4].replace('(', '').replace(') ', '-'))
+                elif '(' in result.select('td.meta_content')[0].text.split('\n\n')[3]:
+                    restaurant_dict['phone'] = clean(result.select('td.meta_content')[0].text.split('\n\n')[3].replace('(', '').replace(') ', '-'))
+                else:
+                    pass
+            except IndexError:
+                pass
+
+            r = requests.get(restaurant_dict['detail_url'])
+            soup = BeautifulSoup(r.content)
+
+            try:
+                restaurant_dict['full_description'] = clean(soup.select('div.description .excerpt')[0].text, breaks=True) + ' ...'
+            except IndexError:
+                restaurant_dict['full_description'] = None
+
+            try:
+                restaurant_dict['hours'] = soup.select('div.restaurant_data_indent')[0].text.replace('\n\n', '\n')
+            except IndexError:
+                restaurant_dict['hours'] = None
+
+            restaurants.append(restaurant_dict)
+
+    with open('data/restaurants.json', 'wb') as writefile:
+        writefile.write(json.dumps(restaurants))
+
+def download_restaurant_html():
+    for star in [3,4,5]:
+        r = requests.get('http://entertainment.accessatlanta.com/search?critic_stars=%s&new=n&sort=0&srad=10.0&srss=250&st=restaurant&st_select=restaurant&swhat=&swhen=&swhere=265+Peachtree+Center+Ave+NE+Atlanta+GA+30303' % star)
+
+        with open('data/%s-stars.html' % star, 'wb') as writefile:
+            writefile.write(r.content)
+
 def download_schedule_html():
     r = requests.get('http://ona13.journalists.org/program/schedule/')
 
--- fabfile.py
+++ fabfile.py
@@ -3,5 +3,15 @@ from fabric.api import *
 import data
 
 def load_schedule():
+	"""
+	Build the schedule JSON.
+	"""
 	data.download_schedule_html()
 	data.parse_schedule_html()
+
+def load_restaurants():
+	"""
+	Build the restaurant JSON.
+	"""
+	data.download_restaurant_html()
+	data.parse_restaurant_html()
