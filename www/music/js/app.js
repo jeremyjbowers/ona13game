9165877fe4b7d01584331bbd6d631f55126822d8
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-10-16 03:38:29 -0400

    DO IT.

---
+++ www/music/js/app.js
@@ -0,0 +1,31 @@
+// Enclosures are good.
+$(document).ready(function(){
+
+  // Load the events JSON.
+  $.ajax('events.json', {
+    dataType: 'json',
+    success: function(events, status, jqXHR){
+      // Do something on success. IN this case, let's loop over the events.
+      // Underscore is wonderful. _.each is underscore's loop construct.
+      // Loops over "events" and make an anonymous function that returns
+      // the individual event as "e", the index as "idx" and a something else.
+      // I'm not actually sure what "list" is. It's in the docs, though!
+      _.each(events, function(e, idx, list){
+
+        // Log to the console. FOR SCIENCE.
+        console.log(e);
+
+        // This is the tricksy part.
+        // Templates are better than string appending.
+        // I stored a template in the HTML in a script tag.
+        // Read about Underscore templates; they are the Lord's work.
+        var compiled = _.template($('script.template-event').html());
+
+        // Append the compiled template to div#info.
+        // Also, compile the template with a variable called 'evnt'
+        // that's just this instance of the event object.
+        $('#info').append(compiled({ 'evnt': e }));
+      });
+    }
+  });
+});
