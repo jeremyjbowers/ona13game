7b151fdcc7df3b4d45fd6d2c2ca29e4d0a534650
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-10-17 08:49:43 -0400

    Adds some fancyness re: buttons. Filters. Counts.

--- www/music/js/app.js
+++ www/music/js/app.js
@@ -13,16 +13,18 @@ $(document).ready(function(){
       _.each(events, function(e, idx, list){
 
         // Log to the console. FOR SCIENCE.
-        console.log(e);
         var date = new Date(e.timestamp*1000);
-        console.log(date);
+
+        var days_of_the_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        e['timestamp_slice'] = date.getDay();
+        e['timestamp_string'] = days_of_the_week[date.getDay()];
 
         // This is the tricksy part.
         // Templates are better than string appending.
         // I stored a template in the HTML in a script tag.
         // Read about Underscore templates; they are the Lord's work.
         var compiled = _.template($('script.template-event').html());
-        
+
 
 
         // Append the compiled template to div#info.
@@ -30,6 +32,39 @@ $(document).ready(function(){
         // that's just this instance of the event object.
         $('#info').append(compiled({ 'evnt': e }));
       });
+
+      // Don't forget to count the cards as a condition of AJAX success.
+      count_visible_cards();
+
     }
   });
+
+  // Dear God, things need to filter.
+  $('button').on('click', function(){
+
+    // Get the day slice from the button. This is like 0 = Sunday, etc.
+    var day_slice = $(this).attr('data-date-slice');
+
+    // Convert that day slice into the class of cards that we want to hide.
+    var day_slice_class = 'div.day-' + day_slice;
+
+    // Hide all cards, since we only want to show certain ones.
+    $('div.card').hide();
+
+    // Show just our event days.
+    $(day_slice_class).show();
+
+    // Count what's visible.
+    count_visible_cards();
+  });
+
+  var count_visible_cards = function(){
+
+    // Find all of the card divs that aren't hidden.
+    var visible_cards = $('div.card').not(':hidden');
+
+    // Write the card text to the H2 we previously made and replace text in there.
+    $('#count-target h2').text(String(visible_cards.length) + ' events happening.');
+  };
+
 });
